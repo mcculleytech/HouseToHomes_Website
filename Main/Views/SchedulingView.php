8a3451999fef09f9d6a034418caeb3e855d5da0e
Grym3038 <156126041+Grym3038@users.noreply.github.com>
2025-04-09 03:13:49 -0500

    working on form

--- Main/Views/SchedulingView.php
+++ Main/Views/SchedulingView.php
@@ -3,12 +3,13 @@
 <div class="flex w-full justify-center h-auto items-center grow">
 
 
-<form action="" class="MyForm drop-shadow-md drop-shadow-black my-8 mx-auto rounded-t-lg">
+<form action="" class="MyForm drop-shadow-md drop-shadow-[#b3a7a2] my-8 mx-auto rounded-t-lg">
     <h1 class="flex lg:text-4xl text-2xl capitalize bg-myPurple w-100 h-15 justify-center items-center rounded-t-lg">Request an Estimate</h1>
-    <div class="mb-5 bg-[]">
-    <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-    <input type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" required />
-  </di</form>
+    <div class="mb-5 bg-My-Darker-Tan justify-center items-center pb-4">
+    <label for="email" class="block mb-3 text-sm font-medium text-black font-sans uppercase mx-3 mt-3">Your email</label>
+    <input type="email" id="email" class="w-9/10 flex mx-3 pb-2  rounded outline-2 outline-[#b3a7a2]" placeholder="name@flowbite.com" required />
+  </div>
+</form>
 
 </div>
 
