2bf4bc12743b86bbb1c640640b722455f0504379
Grym3038 <156126041+Grym3038@users.noreply.github.com>
2025-04-09 02:09:36 -0500

    finished header and footer helpers

--- Main/Views/_partials/footer.php
+++ Main/Views/_partials/footer.php
@@ -5,10 +5,10 @@
  */
 ?>
 
-    <footer class="footer mt-5 p-3 border-top rounded">
-        <div class="container">
+    <footer class="flex fixed bottom-0 py-5 w-full justify-center bg-myFooter">
+        <div class="flex w-full justify-center">
             <p>
-                &copy; <?php echo date('Y'); ?> Made by Yasin Sisto, Will Brouillette, &amp; Luke Hamann
+                &copy; <?php echo date('Y'); ?> Made by Yasin Sisto &amp; Ethan Diaz
             </p>
         </div>
     </footer>
--- Main/Views/_partials/header.php
+++ Main/Views/_partials/header.php
@@ -16,13 +16,13 @@
     <link rel="stylesheet" href="Lib\CSS\styles.css">
 </head>
 <body class="bg-blue">
-    <nav class=" top-0 flex w-full py-5 bg-myPurple caret-transparent">
+    <nav class=" top-0 flex w-full py-5 bg-myNav caret-transparent">
         <div class="shrink  px-2 w-1/3">
-            <img src="Lib/Images/Logo_No_Background.png" alt="Logo" class="inline shrink max-w-12 logo-white">
+            <img src="Lib/Images/Logo_Black.png" alt="Logo" class="inline shrink max-w-12 logo-purple">
         </div>
-        <div class="flex fixed right-0 py-2 w-1/3 justify-between">
-        <a href="#" class="inline-block transition-transform duration-300 hover:scale-110 m-auto text-[#FFFDFC]">Request an Estimate</a>
-        <a href="#" class="inline-block transition-transform duration-300 hover:scale-110 m-auto text-[#FFFDFC]">Write a review</a>
-        <a href="#" class="inline-block transition-transform duration-300 hover:scale-110 m-auto pr-10 text-[#FFFDFC]">Contact us</a>
+        <div class="flex fixed right-0 py-2 w-1/3 justify-between text-2xl">
+        <a href="#" class="inline-block transition-transform duration-300 hover:scale-110 m-auto text-[#534B6F]">Request an Estimate</a>
+        <a href="#" class="inline-block transition-transform duration-300 hover:scale-110 m-auto text-[#534B6F]">Write a review</a>
+        <a href="#" class="inline-block transition-transform duration-300 hover:scale-110 m-auto pr-10 text-[#534B6F]">Contact us</a>
         </div>
     </nav>
