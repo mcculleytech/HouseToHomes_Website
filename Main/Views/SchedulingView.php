b49762e1f294e1f04d4924c4d4ca4e96d6ffc660
Grym3038 <156126041+Grym3038@users.noreply.github.com>
2025-04-09 15:43:28 -0500

    form styling

--- Main/Views/SchedulingView.php
+++ Main/Views/SchedulingView.php
@@ -7,9 +7,9 @@
     <h1 class="flex lg:text-4xl text-2xl capitalize bg-myPurple w-100 h-15 justify-center items-center rounded-t-lg extend-purple-down">Request an Estimate</h1>
     <div class="extend-tan-up mb-5 bg-My-Darker-Tan justify-center items-center pb-4">
     <label for="email" class="block mb-3 text-sm font-medium text-black font-sans uppercase mx-3 mt-3">Your email</label>
-    <input type="email" id="email" class="max-w-9/10 justify-center items-center  px-3 flex mx-3 py-2 leading-tight rounded outline-1 outline-[#b3a7a2]   focus:outline-none" placeholder="Mail@website.com" required />
+    <input type="email" id="email" class=" min-w-50 w-80 tan justify-center items-center  px-3 flex mx-3 py-2 leading-tight rounded outline-1 outline-[#b3a7a2]   focus:outline-none" placeholder="Mail@website.com" required />
   </div>
-</form>z
+</form>
 
 </div>
 
