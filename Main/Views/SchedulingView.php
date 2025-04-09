0377ac33737aa69b221eed407ae8eadba25c0df0
Grym3038 <156126041+Grym3038@users.noreply.github.com>
2025-04-09 15:38:17 -0500

    fixed styling

--- Main/Views/SchedulingView.php
+++ Main/Views/SchedulingView.php
@@ -4,12 +4,12 @@
 
 
 <form action="" class="MyForm drop-shadow-md drop-shadow-[#b3a7a2] my-8 mx-auto rounded-t-lg">
-    <h1 class="flex lg:text-4xl text-2xl capitalize bg-myPurple w-100 h-15 justify-center items-center rounded-t-lg">Request an Estimate</h1>
-    <div class="mb-5 bg-My-Darker-Tan justify-center items-center pb-4">
+    <h1 class="flex lg:text-4xl text-2xl capitalize bg-myPurple w-100 h-15 justify-center items-center rounded-t-lg extend-purple-down">Request an Estimate</h1>
+    <div class="extend-tan-up mb-5 bg-My-Darker-Tan justify-center items-center pb-4">
     <label for="email" class="block mb-3 text-sm font-medium text-black font-sans uppercase mx-3 mt-3">Your email</label>
-    <input type="email" id="email" class="w-9/10 flex mx-3 pb-2  rounded outline-2 outline-[#b3a7a2]" placeholder="name@flowbite.com" required />
+    <input type="email" id="email" class="max-w-9/10 justify-center items-center  px-3 flex mx-3 py-2 leading-tight rounded outline-1 outline-[#b3a7a2]   focus:outline-none" placeholder="Mail@website.com" required />
   </div>
-</form>
+</form>z
 
 </div>
 
