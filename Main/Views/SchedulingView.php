69c59a63096d013427707c39a6fbe9a121d05848
Grym3038 <156126041+Grym3038@users.noreply.github.com>
2025-04-09 23:42:32 -0500

    finished form

--- Main/Views/SchedulingView.php
+++ Main/Views/SchedulingView.php
@@ -3,25 +3,56 @@
 <div class="flex w-full justify-center h-auto items-center ">
 
 
-<form action="" class="inline-block MyForm drop-shadow-md drop-shadow-[#b3a7a2] my-8 w-100 mx-auto rounded-t-lg">
-    <h1 class="flex lg:text-4xl text-2xl capitalize bg-myPurple w-100 h-15 justify-center items-center rounded-t-lg extend-purple-down">Request an Estimate</h1>
-    <div class="pt-5 extend-tan-down mb-5 bg-My-Darker-Tan justify-center items-center pb-4">
-      <label for="name" class="block mb-3 w-80 lg:w-100 text-sm font-medium text-black font-sans uppercase mx-3 mt-3">Full Name</label>
-      <input type="text" id="name" class=" min-w-50 w-80 tan justify-center items-center  px-3 flex mx-3 py-2 leading-tight rounded outline-1 outline-[#b3a7a2]   focus:outline-none" placeholder="John Doe" required />
-    </div>
-    <div class="extend-tan-down mb-5 bg-My-Darker-Tan justify-center items-center pb-4">
-      <label for="email" class="block mb-3 w-80 lg:w-100 text-sm font-medium text-black font-sans uppercase mx-3 mt-3">Email Address</label>
-      <input type="email" id="email" class=" min-w-50 w-80 tan justify-center items-center  px-3 flex mx-3 py-2 leading-tight rounded outline-1 outline-[#b3a7a2]   focus:outline-none" placeholder="Mail@website.com" required />
-    </div>
-    <div class="extend-tan-down mb-5 bg-My-Darker-Tan justify-center items-center pb-4">
-      <label for="phone" class="block mb-3 w-80 lg:w-100 text-sm font-medium text-black font-sans uppercase mx-3 mt-3">Phone Number</label>
-      <input type="phone" id="phone" class=" min-w-50 w-80 tan justify-center items-center  px-3 flex mx-3 py-2 leading-tight rounded outline-1 outline-[#b3a7a2]   focus:outline-none" placeholder="Mail@website.com" required />
-    </div>
-    <div class="extend-tan-down mb-5 bg-My-Darker-Tan justify-center items-center pb-4">
+<form action="" class="inline-block MyForm drop-shadow-md drop-shadow-[#b3a7a2] mt-8 w-full lg:w-100 mb:w-100 mx-auto mb:rounded-lg lg:rounded-t-lg">
+    <h1 class="caret-transparent flex lg:text-4xl text-2xl capitalize bg-myPurple w-full lg:w-100 mb:w-100 h-15 justify-center items-center mb:rounded-t-lg lg:rounded-t-lg extend-purple-down">Request an Estimate</h1>
 
-
-    
+    <div class="extend-tan-down pt-2 mb-5 bg-My-Darker-Tan justify-center items-center pb-4">
+    <label for="name" class="block mb-3 w-80 lg:w-100 text-sm font-medium text-black font-sans uppercase mx-3 mt-3 caret-transparent">Full Name</label>
+    <input  type="text" 
+            id="name" 
+            class=" min-w-50 w-93 bg-transparent tan justify-center items-center
+                    px-3 flex mx-3 py-2 leading-tight rounded outline-1 outline-[#b3a7a2] 
+                    border-transparent focus:border-[#807874] focus:outline-none" 
+            placeholder="John Doe" required />
     </div>
+    <div class="extend-tan-down mb-5 bg-My-Darker-Tan justify-center items-center pb-4">
+      <label for="phone" class="block mb-3 w-93 lg:w-100 text-sm font-medium text-black font-sans uppercase mx-3 mt-3 caret-transparent">Phone Number</label>
+      <input  type="phone" 
+              id="phone" 
+              class=" min-w-50 w-93 bg-transparent tan justify-center items-center  
+                      px-3 flex mx-3 py-2 leading-tight rounded outline-1 outline-[#b3a7a2] 
+                      border-transparent focus:outline-[#807874] " 
+              placeholder="(123)-456-7890" required />
+    </div>
+    <div class="extend-tan-down mb-5 bg-My-Darker-Tan justify-center items-center pb-4">
+      <label for="email" class="block mb-3 w-80 lg:w-100 text-sm font-medium text-black font-sans uppercase mx-3 mt-3 caret-transparent">Email</label>
+      <input  type="email" 
+              id="email" 
+              class=" min-w-50 w-93 bg-transparent tan justify-center items-center  px-3 flex 
+                      mx-3 py-2 leading-tight rounded outline-1 outline-[#b3a7a2] border-transparent 
+                    focus:border-[#807874]  focus:outline-none" 
+              placeholder="Mail@website.com" required />
+    </div>
+    <div class="extend-tan-down mb-5 bg-My-Darker-Tan justify-center items-center pb-4 mb:rounded-b-lg lg:rounded-b-lg">
+      <div class="w-80 mx-3 relative">
+      <div class="mb-6">
+        <label for="message" class="block mb-3 w-93 lg:w-100 text-sm font-medium text-black font-sans uppercase caret-transparent">Your message</label>
+        <textarea id="message" 
+                  rows="4" 
+                  class=" block w-93 p-4 tan text-[#5a5451] border placeholder:text-[#5a5451] 
+                        border-[#b3a7a2] rounded-lg bg-transparent text-base  focus:border-[#807874] focus:ring-transparent
+                          focus:bg-[#e6d7d0]" 
+                  placeholder="Lorem ipsum..."></textarea>
+      </div>
+      </div>
+    </div>
+    <div class="extend-tan-down mb-5 bg-My-Darker-Tan flex justify-center items-center pb-4">
+      <button type="submit" 
+              class=" mb-auto text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none 
+                      focus:ring-blue-300 font-medium rounded-lg text-sm w-93 sm:w-auto px-5 py-2.5 text-center 
+                      dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+    </div>
+
 
 </form>
 
--- Main/Views/_partials/footer.php
+++ Main/Views/_partials/footer.php
@@ -6,7 +6,7 @@
 ?>
 <script src="..\node_modules\flowbite\dist\flowbite.min.js"></script>
 
-    <footer class="flex fixed bottom-0 py-5 w-full justify-center bg-myFooter">
+    <footer class="flex  bottom-0 py-5 w-full justify-center bg-myFooter">
         <div class="flex w-full justify-center">
             <p>
                 &copy; <?php echo date('Y'); ?> Made by Yasin Sisto &amp; Ethan Diaz
