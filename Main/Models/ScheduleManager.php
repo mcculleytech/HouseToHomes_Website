7db5905b56a32723ecce4f445cfa83ebed565f65
Grym3038 <156126041+Grym3038@users.noreply.github.com>
2025-04-17 12:19:41 -0500

    ad

--- Main/Models/ScheduleManager.php
+++ Main/Models/ScheduleManager.php
@@ -6,6 +6,11 @@ $client = new \Google_Client();
 $client->setApplicationName('Google Sheet for schedule');
 $client->setScopes([\Google_Service_Sheets::SPREADSHEETS]);
 $client->setAccessType('offline');
-$client->setauthconf
+$client->setAuthConfig(__DIR__ . '/credentials.json');
+$service = new Google_Service_Sheets($client);
+$spreadsheetID = "1q8P33RtFz-A1963pJ_wqPAsK4AcSA4WyefqOO4AChbs";
+
+$range = "WebsiteInfo!A2:E";
+$response = $service->spreadsheets_values->ge
 
 ?>
