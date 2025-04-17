c31d8078e91d35b64a10884467ebdfd12420f865
Grym3038 <156126041+Grym3038@users.noreply.github.com>
2025-04-16 19:14:22 -0500

    changes to footer

--- Main/Views/_partials/header.php
+++ Main/Views/_partials/header.php
@@ -24,7 +24,7 @@
                 <img src="Lib/Images/Logo_Black.png" alt="Logo" class="inline shrink max-w-12 logo-purple">
             </a>
         </div>
-        <div class="flex right-0 py-2 w-80  md:w-6/7 lg:w-6/7 justify-between text-md  md:text-xl lg:text-xl font-bold ">
+        <div class="flex right-0 py-2 w-80  md:w-6/7 lg:w-6/7 justify-evenly  text-md  md:text-xl lg:text-xl font-bold ">
         <?php $currentAction = $_GET['action'] ?? 'home'; ?>
         <?php if($currentAction == 'home') : ?>
             <a href="?action=RequestEstimate" class="hidden lg:inline-block md:inline-block transition-transform duration-300 hover:scale-110 m-auto  text-[#534B6F]">Request an Estimate</a>
@@ -34,14 +34,14 @@
             <a href="?action=home" class=" lg:hidden md:hidden inline-block transition-transform duration-300 hover:scale-110 m-auto  text-[#534B6F]">Home</a> <p class="lg:hidden md:hidden">|</p>
         <?php endif; ?>
 
-        <a href="?action=reviews" class="hidden lg:inline-block md:inline-block transition-transform duration-300 hover:scale-110 m-auto text-[#534B6F]">Write a review</a>
-        <a href="?action=reviews" class="inline-block lg:hidden md:hidden transition-transform duration-300 hover:scale-110 m-auto text-[#534B6F]">Review</a> <p class="lg:hidden md:hidden">|</p>
+        <a href="https://g.page/r/CWpYvnZqvDsDEBM/review" target="_blank"  class="hidden lg:inline-block md:inline-block transition-transform duration-300 hover:scale-110 m-auto text-[#534B6F]">Write a review</a>
+        <a href="https://g.page/r/CWpYvnZqvDsDEBM/review" target="_blank"  class="inline-block lg:hidden md:hidden transition-transform duration-300 hover:scale-110 m-auto text-[#534B6F]">Review</a> <p class="lg:hidden md:hidden">|</p>
 
-        <a href="?action=contact" class="hidden lg:inline-block md:inline-block transition-transform duration-300 hover:scale-110 m-auto pr-10 text-[#534B6F]">Contact us</a>
-        <a href="?action=contact" class="inline-block lg:hidden md:hidden transition-transform duration-300 hover:scale-110 m-auto md:pr-10 text-[#534B6F]">Contact</a>
+        <a href="?action=contact" class="hidden lg:inline-block md:inline-block transition-transform duration-300 hover:scale-110 m-auto  text-[#534B6F]">Contact us</a>
+        <a href="?action=contact" class="inline-block lg:hidden md:hidden transition-transform duration-300 hover:scale-110 m-auto text-[#534B6F]">Contact</a>
 
-        <a href="?action=about" class="hidden lg:inline-block md:inline-block transition-transform duration-300 hover:scale-110 m-auto pr-10 text-[#534B6F]">About us</a>
-        <a href="?action=about" class="inline-block lg:hidden md:hidden transition-transform duration-300 hover:scale-110 m-auto md:pr-10 text-[#534B6F]">About</a>
+        <a href="?action=about" class="hidden lg:inline-block md:inline-block transition-transform duration-300 hover:scale-110 m-auto  text-[#534B6F]">About us</a>
+        <a href="?action=about" class="inline-block lg:hidden md:hidden transition-transform duration-300 hover:scale-110 m-auto text-[#534B6F]">About</a>
         </div>
     </nav>
 
