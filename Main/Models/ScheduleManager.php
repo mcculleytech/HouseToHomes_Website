d820c4dbdd89bc98074a7c8ba20d41116e74e049
Grym3038 <156126041+Grym3038@users.noreply.github.com>
2025-04-17 13:31:23 -0500

    as

--- Main/Models/ScheduleManager.php
+++ Main/Models/ScheduleManager.php
@@ -1,16 +1,44 @@
 <?php 
 
-require __DIR__ . 'vendor/autoload.php';
+require  '..\..\vendor\autoload.php';
 
 $client = new \Google_Client();
 $client->setApplicationName('Google Sheet for schedule');
 $client->setScopes([\Google_Service_Sheets::SPREADSHEETS]);
 $client->setAccessType('offline');
-$client->setAuthConfig(__DIR__ . '/credentials.json');
+$client->setAuthConfig('..\..\credentials.json');
 $service = new Google_Service_Sheets($client);
 $spreadsheetID = "1q8P33RtFz-A1963pJ_wqPAsK4AcSA4WyefqOO4AChbs";
 
-$range = "WebsiteInfo!A2:E";
-$response = $service->spreadsheets_values->ge
+$range = "WebsiteInfo";
+
+$values = [
+    $_POST['name'],  $_POST['phone'], $_POST['email'], $_POST['message']
+];
+
+try{
+    $body = new Google_Service_Sheets_ValueRange([
+    'values' => $values
+    ]);
+    $params = [
+    'valueInputOption' => 'RAW'
+    ];
+    $insert = [
+        "insertDataOption" => "INSERT_ROWS"
+    ];
+        //executing the request
+        $result = $service->spreadsheets_values->append(
+            $spreadsheetID, 
+            $range, 
+            $body, 
+            $params,
+            $insert);
+        printf("%d cells updated.", $result->getUpdatedCells());
+    return $result;
+}
+catch(Exception $e) {
+    echo 'Message: ' .$e->getMessage();
+}
+
 
 ?>
--- Main/Views/SchedulingView.php
+++ Main/Views/SchedulingView.php
@@ -3,13 +3,14 @@
 <div class="main-content flex w-full justify-center h-auto items-center ">
 
 
-<form action="" class="inline-block MyForm drop-shadow-md drop-shadow-[#b3a7a2] mt-8 mb-13 w-full lg:w-100 md:w-100 mx-auto md:rounded-lg lg:rounded-t-lg">
+<form action="Models\ScheduleManager.php" method="POST" class="inline-block MyForm drop-shadow-md drop-shadow-[#b3a7a2] mt-8 mb-13 w-full lg:w-100 md:w-100 mx-auto md:rounded-lg lg:rounded-t-lg">
     <h1 class="caret-transparent flex lg:text-4xl text-2xl capitalize bg-myPurple w-full lg:w-100 md:w-100 h-15 py-10 justify-center items-center md:rounded-t-lg lg:rounded-t-lg extend-purple-down">Request an Estimate</h1>
 
     <div class="extend-tan-down pt-2 mb-5 bg-My-Tan justify-center items-center pb-4">
     <label for="name" class="block mb-3 w-80 lg:w-100 text-sm font-medium text-black font-sans uppercase mx-3 mt-3 caret-transparent">Full Name</label>
     <input  type="text" 
             id="name" 
+            name="name"
             class=" min-w-50 w-93 bg-transparent tan justify-center items-center
                     px-3 flex mx-3 py-2 leading-tight rounded outline-1 outline-[#b3a7a2] 
                     border-transparent focus:border-[#807874] focus:outline-none" 
@@ -19,6 +20,7 @@
       <label for="phone" class="block mb-3 w-93 lg:w-100 text-sm font-medium text-black font-sans uppercase mx-3 mt-3 caret-transparent">Phone Number</label>
       <input  type="phone" 
               id="phone" 
+              name="phone"
               class=" min-w-50 w-93 bg-transparent tan justify-center items-center  
                       px-3 flex mx-3 py-2 leading-tight rounded outline-1 outline-[#b3a7a2] 
                       border-transparent focus:outline-[#807874] " 
@@ -28,6 +30,7 @@
       <label for="email" class="block mb-3 w-80 lg:w-100 text-sm font-medium text-black font-sans uppercase mx-3 mt-3 caret-transparent">Email</label>
       <input  type="email" 
               id="email" 
+              name="email"
               class=" min-w-50 w-93 bg-transparent tan justify-center items-center  px-3 flex 
                       mx-3 py-2 leading-tight rounded outline-1 outline-[#b3a7a2] border-transparent 
                     focus:border-[#807874]  focus:outline-none" 
@@ -42,6 +45,7 @@
         ?>
         <textarea id="message" 
                   rows="4" 
+                  name="message"
                   class=" block w-93 p-4 tan text-[#5a5451] border placeholder:text-[#5a5451] 
                         border-[#b3a7a2] rounded-lg bg-transparent text-base  focus:border-[#807874] focus:ring-transparent
                           focus:bg-[#e6d7d0]" 
