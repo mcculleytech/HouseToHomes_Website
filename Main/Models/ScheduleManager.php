07bf47393b9bc1f4dfea601275d0f592c67c8507
Grym3038 <156126041+Grym3038@users.noreply.github.com>
2025-04-17 11:57:33 -0500

    google stuff

--- Main/Models/ScheduleManager.php
+++ Main/Models/ScheduleManager.php
@@ -1,37 +1,11 @@
 <?php 
 
-use Google\Client;
-use Google\Service\Drive;
-use Google\Service\Sheets\ValueRange;
+require __DIR__ . 'vendor/autoload.php';
 
+$client = new \Google_Client();
+$client->setApplicationName('Google Sheet for schedule');
+$client->setScopes([\Google_Service_Sheets::SPREADSHEETS]);
+$client->setAccessType('offline');
+$client->setauthconf
 
-function updateValues($spreadsheetId, $range, $valueInputOption)
-    {
-        /* Load pre-authorized user credentials from the environment.
-           TODO(developer) - See https://developers.google.com/identity for
-            guides on implementing OAuth2 for your application. */
-        $client = new Google\Client();
-        $client->useApplicationDefaultCredentials();
-        $client->addScope(Google\Service\Drive::DRIVE);
-        $service = new Google_Service_Sheets($client);
-        try{
-        $values = [["sample", 'values']];
-
-        $body = new Google_Service_Sheets_ValueRange([
-            'values' => $values
-        ]);
-        $params = [
-            'valueInputOption' => $valueInputOption
-        ];
-        //executing the request
-        $result = $service->spreadsheets_values->update($spreadsheetId, $range,
-        $body, $params);
-        printf("%d cells updated.", $result->getUpdatedCells());
-        return $result;
-    }
-    catch(Exception $e) {
-            // TODO(developer) - handle error appropriately
-            echo 'Message: ' .$e->getMessage();
-          }
-    }
 ?>
