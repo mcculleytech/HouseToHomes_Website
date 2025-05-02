023db08dc63ee27bf78dc5104dfaa4191fecce28
Grym3038 <156126041+Grym3038@users.noreply.github.com>
2025-05-01 23:16:02 -0500

    Update 5/1/2025
    changed about.php text so that it is correct.
    commented out team member list.
    added days and hours selection to scheduling form.

--- Main/Models/ScheduleManager.php
+++ Main/Models/ScheduleManager.php
@@ -13,9 +13,19 @@ $service = new Google_Service_Sheets($client);
 $spreadsheetID = "1q8P33RtFz-A1963pJ_wqPAsK4AcSA4WyefqOO4AChbs";
 
 $range = "ContactInfo!A2:D";
+$selectedDays  = $_POST['days']  ?? [];
+$selectedTimes = $_POST['hours'] ?? [];
+
+$daysString  = count($selectedDays)
+  ? implode(', ', $selectedDays)
+  : 'None selected';
+$timesString = count($selectedTimes)
+  ? implode(', ', $selectedTimes)
+  : 'None selected';
 
 $values = [
-    $_POST['name'],  $_POST['phone'], $_POST['email'], $_POST['message']
+    $_POST['name'],  $_POST['phone'], $_POST['email'], $_POST['message'],    $daysString,
+    $timesString
 ];
 
 try{
--- Main/Views/About.php
+++ Main/Views/About.php
@@ -5,25 +5,33 @@
 <div  class="grid grid-cols-3 justify-items-center text-black gap-10 lg:w-[80%] m-auto">
   <div  class=" mt-10 md:mt-0 lg:mt-0 col-span-3 lg:col-span-1 p- lg:order-2 justify-items-center content-center ">
     <h1 class="text-6xl">About Us</h1>
-    <p class="text-lg px-4 "> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vitae, illum! Obcaecati recusandae fugiat mollitia sit quas assumenda ad id. Mollitia debitis minima praesentium aliquam odit corrupti ipsam asperiores saepe quas.</p>
+    <p class="text-lg px-4 ">
+      House to Home Solutions was founded in 2024 with one simple mission: to help your house truly feel like home. 
+      Whether it’s fixing what’s broken, refreshing what’s worn, or installing something brand new, we’re here to help every step of the way. 
+      From repairs and renovations to custom installations and more, we take pride in reliable service, quality craftsmanship, and making your vision a reality—one project at a time. 
+      Let’s turn your house into the home you’ve always dreamed of.
+    </p>
   </div>
   <div class="col-span-3 md:col-span-2 lg:col-span-2 lg:order-1 w-full p-4 px-5 ">
     <img class="rounded-2xl h-[30rem] w-full md:w-[80%] lg:w-[100%] object-cover shadow-md" src="Lib\Images\DSC_8913.jpg" alt="DSC_8913.jpg">
   </div>
-  <div class=" lg:order-3 p-4 col-span-3 md:col-span-2 lg:col-span-2 justify-items-center content-center py-20">
+  <div class=" lg:order-3 p-4 col-span-3 md:col-span-2 lg:col-span-2 justify-items-center content-center py-20 mb-20 " style="padding-bottom:200px">
     <h1 class="text-6xl">Our Mission</h1>
-    <p class="text-lg px-4 text-center">Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
-Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.</p>
+    <p class="text-lg px-4 text-center">
+    Charlie has always had a natural talent for fixing what’s broken and improving what can be better. 
+    That passion for problem-solving and craftsmanship is what inspired him to create House to Home Solutions. 
+    Behind every project whether big or small is reliable repairs, crafted with integrity.
+    </p>
   </div>
   <div class=" hidden md:block lg:block lg:order-4 p-4 relative  w-full">
     <img class="absolute top-0 left-0 md:top-0 lg:top-[-7rem] md:left-0 lg:left-[-5%] shadow-md rounded-2xl h-[35rem] w-[50rem] object-cover " src="Lib\Images\DSC_8844.jpg" alt="DSC_8844.jpg">
   </div>
     <!-- Dark Tan bar -->
-
+<!-- 
   <div class="col-span-3 lg:order-5 bg-[#e6d7d0] w-full h-[2px]"></div>
-  <div class="text-6xl col-span-3 lg:order-6 p-4">Our Team</div>
+  <div class="text-6xl col-span-3 lg:order-6 p-4">Our Team</div> -->
   <!-- Team Memer #1 -->
-  <div class="col-span-3 md:col-span-1 lg:col-span-1 grid grid-cols-2 lg:order-7 p-4">
+  <!-- <div class="col-span-3 md:col-span-1 lg:col-span-1 grid grid-cols-2 lg:order-7 p-4">
     <div class="justify-items-center content-center">
          <img class="bg-[#e6d7d0] rounded-full aspect-1/1 h-[70%] g:w-auto  object-cover shadow-md" src="Lib\Images\Charlie_Face_image.png" alt="Charlie_Face_image.png">
     </div>
@@ -31,10 +39,10 @@ Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapi
       <h1 class="text-3xl font-bold">Charlie Tibbetts</h1>
       <p class="text-sm font-semibold text-[#00000073] text-left">Owner</p>
     </div>
-  </div>
+  </div> -->
     <!-- Team Memer #2 -->
 
-  <div class="col-span-3 md:col-span-1 lg:col-span-1  grid grid-cols-2 lg:order-8 p-4 w-full h-full">
+  <!-- <div class="col-span-3 md:col-span-1 lg:col-span-1  grid grid-cols-2 lg:order-8 p-4 w-full h-full">
     <div class="justify-items-center content-center px-5 h-full">
          <img class="bg-[#e6d7d0] rounded-full aspect-1/1 h-[70%] g:w-auto  object-cover shadow-md fill-white" src="Lib\Images\person_24dp_736968_FILL0_wght400_GRAD0_opsz24.svg"  alt="Person.png">
     </div>
@@ -42,10 +50,10 @@ Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapi
       <h1 class="text-3xl font-bold">Jane Doe</h1>
       <p class="text-sm font-semibold text-[#00000073] text-left">Marketing Manager</p>
     </div>
-  </div>
+  </div> -->
   <!-- Team Memer #3 -->
 
-  <div class="col-span-3 md:col-span-1 lg:col-span-1 grid grid-cols-2 lg:order-9 p-4 w-full h-full">
+  <!-- <div class="col-span-3 md:col-span-1 lg:col-span-1 grid grid-cols-2 lg:order-9 p-4 w-full h-full">
     <div class="justify-items-center content-center px-5">
          <img class="bg-[#e6d7d0] rounded-full aspect-1/1 h-[70%] g:w-auto  object-cover shadow-md fill-white" src="Lib\Images\person_24dp_736968_FILL0_wght400_GRAD0_opsz24.svg"  alt="Person.png">
     </div>
@@ -53,7 +61,7 @@ Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapi
       <h1 class="text-3xl font-bold">John Doe</h1>
       <p class="text-sm font-semibold text-[#00000073] text-left">Head Of Design</p>
     </div>
-  </div>
+  </div> -->
 </div>
 
 
