ae5d458ddfc0603717e82f6defeed274e2a7d80a
Grym3038 <156126041+Grym3038@users.noreply.github.com>
2025-05-28 02:50:28 -0500

    fixed footer and removed contact from home page

--- Main/Views/IndexView.php
+++ Main/Views/IndexView.php
@@ -114,34 +114,6 @@
 
 
 
-<!-- Contact Info Section -->
-<div class="w-full mt-4 mb-10 flex flex-col items-center gap-4 text-center text-[#005887] font-sans bg-My-Tan py-6 px-4 rounded-md ">
 
-<!-- Phone -->
-<div class="flex items-center gap-2">
-    <svg class="w-6 h-6 text-myPurple" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24">
-        <path stroke-linecap="round" stroke-linejoin="round"
-            d="M3 5a2 2 0 012-2h1.586a1 1 0 01.707.293l2.828 2.828a1 1 0 010 1.414L8.414 9.586a16.021 16.021 0 006 6l2.051-2.051a1 1 0 011.414 0l2.828 2.828a1 1 0 01.293.707V19a2 2 0 01-2 2h-1c-9.389 0-17-7.611-17-17V5z" />
-    </svg>
-    <span class="text-base">Call us: <a href="tel:+6054004440" class="text-myPurple underline">(605) 400-4440</a></span>
-</div>
-
-<!-- Facebook -->
-<div class="flex items-center gap-2">
-    <svg class="w-6 h-6 text-myPurple" fill="currentColor" viewBox="0 0 24 24">
-        <path d="M22 12c0-5.522-4.477-10-10-10S2 6.478 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.988H7.898v-2.89h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.772-1.63 1.562v1.875h2.773l-.443 2.89h-2.33V21.878C18.343 21.128 22 16.991 22 12z" />
-    </svg>
-    <span class="text-base">Follow us on <a href="https://www.facebook.com/people/House-to-Home-Solutions/61567396322695/" target="_blank" class="text-myPurple underline">Facebook</a></span>
-</div>
-
-<!-- Instagram -->
-<div class="flex items-center gap-2">
-    <svg class="w-6 h-6 text-myPurple" fill="currentColor" viewBox="0 0 24 24">
-        <path d="M7.75 2C4.574 2 2 4.574 2 7.75v8.5C2 19.426 4.574 22 7.75 22h8.5C19.426 22 22 19.426 22 16.25v-8.5C22 4.574 19.426 2 16.25 2h-8.5zM12 7.25a4.75 4.75 0 110 9.5 4.75 4.75 0 010-9.5zM18.25 5.75a1 1 0 110 2 1 1 0 010-2zM12 9.25a2.75 2.75 0 100 5.5 2.75 2.75 0 000-5.5z" />
-    </svg>
-    <span class="text-base">Find us on <a href="https://www.instagram.com/h2h.solutions/#" target="_blank" class="text-myPurple underline">Instagram</a></span>
-</div>
-
-</div>
 
 <?php include('Views/_partials/footer.php'); ?>
